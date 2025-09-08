642035d8fc934a1c24d85dd5fa7ac209f54e069c
DhruvPandey1 <115931661+DhruvPandey1@users.noreply.github.com>
2025-09-09 01:27:16 +0530

    Auth logic

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,15 +4,15 @@ const generateToken = require("../utils/generateToken");
 
 exports.registerUser = async (req, res) => {
   console.log(req.body)
-  const { username, password } = req.body;
+  const { name ,email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await User.create({ username, password: hashedPassword });
+  const user = await User.create({ username:name, password: hashedPassword, email:email });
   res.json({ token: generateToken(user.id) });
 };
 
 exports.loginUser = async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ where: { username } });
+  const { name, password } = req.body;
+  const user = await User.findOne({ where: { username:name } });
   if (user && (await bcrypt.compare(password, user.password))) {
     res.json({ token: generateToken(user.id) });
   } else {
--- backend/models/User.js
+++ backend/models/User.js
@@ -4,6 +4,7 @@ const sequelize = require("../config/db");
 const User = sequelize.define("User", {
   username: { type: DataTypes.STRING, allowNull: false, unique: true },
   password: { type: DataTypes.STRING, allowNull: false },
+  email: {type: DataTypes.STRING, allowNull: false, unique: true, validate:{isEmail:{msg:"Must be a valid email address"},},},
 });
 
 module.exports = User;

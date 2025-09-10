1c485481426f5570de1aaab8849e89ceb3e96bf1
DhruvPandey1 <115931661+DhruvPandey1@users.noreply.github.com>
2025-09-11 01:28:39 +0530

    test

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
 const User = sequelize.define("User", {
-  username: { type: DataTypes.STRING, allowNull: false, unique: true },
+  username: { type: DataTypes.STRING, allowNull: true, unique: true },
   password: { type: DataTypes.STRING, allowNull: false },
   email: {type: DataTypes.STRING, allowNull: false, unique: true, validate:{isEmail:{msg:"Must be a valid email address"},},},
 });
